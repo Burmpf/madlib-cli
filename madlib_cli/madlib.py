ee2877c2cbbafec3eeac39e04c22ee892ff88d4a
Keyan <keyantabor98@gmail.com>
2022-12-21 21:02:26 -0500

    first commit

---
+++ madlib_cli/madlib.py
@@ -0,0 +1,15 @@
+
+intro = print(
+"""
+****************************************
+**       Words, They mean things      **
+** Unfortunately you have to use them **
+**         This is a madlib.          **
+**       put the words in the holes   **
+**      Wait, that sounds weird       ** 
+**       You knew what I meant        **
+**     We all know how madlibs work   **
+**                                    **
+****************************************
+""")
+
